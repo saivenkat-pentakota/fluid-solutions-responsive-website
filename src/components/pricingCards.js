fca10bca3bff5706b6ca8fa5d331a140cb0a7064
Saivenkat Pentakota <saivenkatpentakota@gmail.com>
2023-08-10 16:47:52 +0530

    modified the code

--- src/components/pricingCards.js
+++ src/components/pricingCards.js
@@ -3,9 +3,15 @@ import pricingCard from "../images/pricing cards.jpg";
 
 function pricingCards() {
   return (
-
-    <div className="pricingCards d-flex justify-content-center m-5">
-      <img src = {pricingCard}  alt = "pricing card"/>
+    <div>
+      <div className=" d-flex m-3 justify-content-center">
+        <p className="p-3" style={{fontSize:"20px"}}> 
+          Choose the best pricing for start your journey
+        </p>
+      </div>
+      <div className="pricingCards d-flex justify-content-center m-5">
+        <img src={pricingCard} alt="pricing card" />
+      </div>
     </div>
   );
 }
