19c5ac43e9b5e5fe65ed209090a8a015e0952aea
Saivenkat Pentakota <saivenkatpentakota@gmail.com>
2023-08-10 21:18:08 +0530

    updated pricing card width and height

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -4,7 +4,7 @@ function pricing() {
   return (
     <div className=" d-flex flex-row justify-content-center m-5">
         {/* card-1 */}
-      <div class="card d-flex flex-column" style={{width:"250px" ,height:"380px"}}>
+      <div class="card d-flex flex-column m-3" style={{width:"250px" ,height:"380px"}}>
         <div className="card-top-portion p-3 " style={{backgroundColor:"lightgreen"}}>
           <div className="d-flex justify-content-end">
             <h5>Basic</h5>
@@ -24,44 +24,44 @@ function pricing() {
       </div>
 
       {/* card-2 */}
-      {/* <div class="card m-3 ">
-        <div className="card-top-portion p-4 " style={{backgroundColor:"lightgreen"}}>
+      <div class="card d-flex flex-column m-3" style={{width:"250px" ,height:"380px"}}>
+        <div className="card-top-portion p-3 " style={{backgroundColor:"lightgreen"}}>
           <div className="d-flex justify-content-end">
-            <h5>Standard</h5>
+            <h5>Basic</h5>
           </div>
-          <h1>20$</h1>
+          <h1>10$</h1>
           <p>USD/Month</p>
           <p>For more details <br></br>click here</p>
         </div>
 
-        <div class="card-body p-4 ">
-          <p class="card-title"> perfect for Intermediate</p>
+        <div class="d-flex flex-column card-body" style={{textAlign:"center"}}>
+          <p class="card-title">perfect for beginners!</p>
           <p class="card-text">subscription details</p>
           <button className="btn bg-primary" style={{ color: "white" }}>
             subscribe now
           </button>
         </div>
-      </div> */}
+      </div>
 
       {/* card-3 */}
-      {/* <div class="card m-3 ">
-        <div className="card-top-portion p-4 " style={{backgroundColor:"lightgreen"}}>
+      <div class="card d-flex flex-column m-3" style={{width:"250px" ,height:"380px"}}>
+        <div className="card-top-portion p-3 " style={{backgroundColor:"lightgreen"}}>
           <div className="d-flex justify-content-end">
-            <h5>Premium</h5>
+            <h5>Basic</h5>
           </div>
-          <h1>30$</h1>
+          <h1>10$</h1>
           <p>USD/Month</p>
           <p>For more details <br></br>click here</p>
         </div>
 
-        <div class="card-body p-4">
-          <p class="card-title">Unlimited access </p>
+        <div class="d-flex flex-column card-body" style={{textAlign:"center"}}>
+          <p class="card-title">perfect for beginners!</p>
           <p class="card-text">subscription details</p>
           <button className="btn bg-primary" style={{ color: "white" }}>
             subscribe now
           </button>
         </div>
-      </div> */}
+      </div>
       
       
     </div>
